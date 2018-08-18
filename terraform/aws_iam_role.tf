44908541e740c42d4b42ac26bcad8441d04d8b70
ahasegawa <a.hasegawa.dw@gmail.com>
2018-08-18 15:07:56 +0900

    iamの設定を追加

---
+++ terraform/aws_iam_role.tf
@@ -0,0 +1,58 @@
+resource "aws_iam_instance_profile" "instance_role" {
+  name  = "ecs_instance_role"
+  roles = ["${aws_iam_role.ecs_instance_role.name}"]
+}
+
+resource "aws_iam_role" "ecs_instance_role" {
+  name = "ecs_instance_role"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "ecs_service_role" {
+  name = "ecs_service_role"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "",
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "ecs.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  }
+  EOF
+}
+
+resource "aws_iam_policy_attachment" "ecs_instance_policy" {
+  name       = "ecs_instance_policy"
+  roles      = ["${aws_iam_role.ecs_instance_role.name}"]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_policy_attachment" "ecs_service_policy" {
+  name       = "ecs_service_policy"
+  roles      = ["${aws_iam_role.ecs_service_role.name}"]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
+}
